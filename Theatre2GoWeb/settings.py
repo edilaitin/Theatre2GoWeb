2bc1f1b890026260230f2beba01242053b4a9f23
Ana Oravitan <anamaria.oravitan@gmail.com>
2021-01-15 21:56:08 +0200

    nav_bar

--- Theatre2GoWeb/settings.py
+++ Theatre2GoWeb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import cloudinary
@@ -167,6 +167,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
--- Theatre2GoWeb/urls.py
+++ Theatre2GoWeb/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="login/index.html")),
@@ -26,3 +27,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),  # Keep as last
 ]
+
+urlpatterns += staticfiles_urlpatterns()
