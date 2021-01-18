abcc8669338bfb6f648cd2551bd95692f05585b3
edilaitin <33549017+edilaitin@users.noreply.github.com>
2021-01-19 01:11:52 +0200

    Add full text search on multiple fields for plays/theatres/artists
    haha postgres go brrrr

--- plays/views.py
+++ plays/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 from django.views.generic import ListView, DetailView
 
 from plays.models import Play
@@ -6,6 +7,30 @@ from plays.models import Play
 class PlayListView(ListView):
     model = Play
     context_object_name = 'plays'
+
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            queryset = Play.objects.all()
+
+            keywords = self.request.GET.get('q')
+            if keywords:
+                query = SearchQuery(keywords)
+                title_vector = SearchVector('vaultPlay__title', weight='A')
+                theatre_vector = SearchVector('vaultPlay__theatre__name', weight='B')
+                vectors = title_vector + theatre_vector
+                queryset = queryset.annotate(rank=SearchRank(vectors, query)) \
+                    .filter(rank__gte=0.2).order_by('-rank').distinct()
+
+            return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        keywords = self.request.GET.get('q')
+        if keywords:
+            context['query'] = keywords
+
+        return context
 
 
 class PlayDetailView(DetailView):
--- theatres/views.py
+++ theatres/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 from django.views.generic import ListView, DetailView
 
 from theatres.models import Theatre
@@ -10,9 +11,19 @@ class TheatreListView(ListView):
     def get_queryset(self):
         if self.request.method == 'GET':
             queryset = Theatre.objects.all()
-            theatre_type = self.request.GET.get('type', None)
-            if theatre_type is not None:
+            theatre_type = self.request.GET.get('type')
+            if theatre_type:
                 queryset = queryset.filter(theatreType__type__exact=theatre_type).distinct()
+
+            keywords = self.request.GET.get('q')
+            if keywords:
+                query = SearchQuery(keywords)
+                name_vector = SearchVector('name', weight='A')
+                artist_vector = SearchVector('artist__name', weight='B')
+                vectors = name_vector + artist_vector
+                queryset = queryset.annotate(rank=SearchRank(vectors, query)) \
+                    .filter(rank__gte=0.3).order_by('-rank').distinct()
+
             return queryset
 
     def get_context_data(self, **kwargs):
@@ -20,6 +31,11 @@ class TheatreListView(ListView):
         theatre_type = self.request.GET.get('type', None)
         if theatre_type is not None:
             context['type'] = theatre_type
+
+        keywords = self.request.GET.get('q')
+        if keywords:
+            context['query'] = keywords
+
         return context
 
 
