1e2c733310447d0c76536a311cab7ee49ad3d870
edilaitin <33549017+edilaitin@users.noreply.github.com>
2021-01-19 01:11:52 +0200

    Add free text search on multiple fields for play/theatres/artists
    haha postres go brrrrr

--- Theatre2GoWeb/settings.py
+++ Theatre2GoWeb/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.postgres',  # for fts search
 
     'phonenumber_field',
     'location_field.apps.DefaultConfig',
--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,5 @@
+from django.contrib.postgres.aggregates import StringAgg
+from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
 from django.views.generic import ListView, DetailView
 
 from artists.models import Artist
@@ -16,6 +18,17 @@ class ArtistListView(ListView):
                     queryset = queryset.filter(actorrole__isnull=False).distinct()
                 elif artist_type == "director":
                     queryset = queryset.filter(director__isnull=False).distinct()
+
+            keywords = self.request.GET.get('q')
+            if keywords:
+                query = SearchQuery(keywords)
+                name_vector = SearchVector('name', weight='A')
+                theatre_vector = SearchVector(StringAgg('associatedTheatres__name', delimiter=' '), weight='B')
+                play_vector = SearchVector(StringAgg('actorrole__vaultPlay__title', delimiter=' '), weight='B')
+                vectors = name_vector + theatre_vector + play_vector
+                queryset = queryset.annotate(rank=SearchRank(vectors, query)) \
+                    .filter(rank__gte=0.2).order_by('-rank').distinct()
+
             return queryset
 
     def get_context_data(self, **kwargs):
@@ -23,6 +36,11 @@ class ArtistListView(ListView):
         artist_type = self.request.GET.get('type', None)
         if artist_type is not None:
             context['type'] = artist_type
+
+        keywords = self.request.GET.get('q')
+        if keywords:
+            context['query'] = keywords
+
         return context
 
 
